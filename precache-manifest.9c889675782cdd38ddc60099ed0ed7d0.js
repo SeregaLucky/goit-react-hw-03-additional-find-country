7b265507f4cfdd1935c91167a931a20520b4546f
Sergii Stryzg <strizgsergey@gmail.com>
2019-10-30 12:09:53 +0200

    Updates

---
+++ precache-manifest.9c889675782cdd38ddc60099ed0ed7d0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b56ef5ef7d63efced60c91c61f36dc5",
+    "url": "/goit-react-hw-03-additional-find-country/index.html"
+  },
+  {
+    "revision": "ae690824e570d016ea1d",
+    "url": "/goit-react-hw-03-additional-find-country/static/css/2.5dbdccff.chunk.css"
+  },
+  {
+    "revision": "2b9ab0132e1ef08b9df8",
+    "url": "/goit-react-hw-03-additional-find-country/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "ae690824e570d016ea1d",
+    "url": "/goit-react-hw-03-additional-find-country/static/js/2.23877df4.chunk.js"
+  },
+  {
+    "revision": "2b9ab0132e1ef08b9df8",
+    "url": "/goit-react-hw-03-additional-find-country/static/js/main.9441adb0.chunk.js"
+  },
+  {
+    "revision": "cd22873acadfe709c103",
+    "url": "/goit-react-hw-03-additional-find-country/static/js/runtime-main.ba8b8f59.js"
+  }
+]);
